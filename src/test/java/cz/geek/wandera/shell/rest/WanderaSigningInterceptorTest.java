8a84259ffd8b2ddad2c0cf7b0e3836fe1af99f2d
Martin Caslavsky <martin.caslavsky@wandera.com>
2018-04-09 17:41:52 +0200

    add: interceptor test

---
+++ src/test/java/cz/geek/wandera/shell/rest/WanderaSigningInterceptorTest.java
@@ -0,0 +1,51 @@
+package cz.geek.wandera.shell.rest;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+@RunWith(SpringRunner.class)
+@RestClientTest(RestService.class)
+public class WanderaSigningInterceptorTest {
+
+	@Autowired
+	private MockRestServiceServer server;
+
+	@Autowired
+	private RestService service;
+
+	@Test
+	public void shouldAddHeaders() throws Exception {
+		server.expect(requestTo("http://localhost/test"))
+				.andExpect(header("X-Sig", is(notNullValue())))
+				.andExpect(header("X-Ts", is("3600000")))
+				.andExpect(header("X-Key", is("api")))
+				.andRespond(withSuccess());
+
+		service.get("http://localhost/test", String.class);
+	}
+
+	static class Config {
+
+		@Bean WanderaSigningInterceptor interceptor() {
+			WanderaKeys keys = new WanderaKeys("api", "secret");
+			Clock clock = Clock.fixed(Instant.ofEpochSecond(3600), ZoneId.systemDefault());
+			return new WanderaSigningInterceptor(keys, clock);
+		}
+
+	}
+}
