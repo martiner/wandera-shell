985cb0fcc1c8507bd944531d908eb6a11826b87a
Martin Caslavsky <martin.caslavsky@wandera.com>
2019-10-18 22:43:00 +0200

    chg: style prompt a bit

--- src/main/java/cz/geek/wandera/shell/WanderaShellPromptProvider.java
+++ src/main/java/cz/geek/wandera/shell/WanderaShellPromptProvider.java
@@ -3,6 +3,7 @@ package cz.geek.wandera.shell;
 import java.util.Optional;
 
 import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
 import org.jline.utils.AttributedStyle;
 import org.springframework.shell.jline.PromptProvider;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,8 @@ import cz.geek.wandera.shell.rest.RestService;
 @Component
 public class WanderaShellPromptProvider implements PromptProvider {
 
-	private static final AttributedStyle STYLE = AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW);
+	private static final AttributedStyle STYLE = AttributedStyle.DEFAULT.foreground(AttributedStyle.YELLOW).bold();
+	private static final AttributedStyle BLUE = AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE);
 
 	private final KeysHolder holder;
 	private final RestService service;
@@ -25,17 +27,18 @@ public class WanderaShellPromptProvider implements PromptProvider {
 
 	@Override
 	public AttributedString getPrompt() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("wnd:");
+		AttributedStringBuilder builder = new AttributedStringBuilder();
+		builder.append("wnd:", STYLE);
 		if (holder.hasKeys()) {
 			String keyName = Optional.ofNullable(holder.getKeys().getName()).orElse("(unnamed)");
-			builder.append(keyName).append(":");
+			builder.append(keyName, BLUE)
+					.append(":", STYLE);
 		}
 		if (service.hasLastUri()) {
-			builder.append(service.getLastUri());
+			builder.append(service.getLastUri().toString());
 		}
 
-		builder.append("> ");
-		return new AttributedString(builder, STYLE);
+		builder.append("> ", STYLE);
+		return builder.toAttributedString();
 	}
 }
